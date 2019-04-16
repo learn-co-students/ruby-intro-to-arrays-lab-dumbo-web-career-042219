f474488546cef1d2698b7641141c640df65b9071
Chloe <gingkolane@gmail.com>
2019-04-16 17:16:13 -0400

    Update intro_to_arrays.rb

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def my_new_array
+  my_new_array = []
+end
+
+def my_two_array
+  my_two_array = ['one', 'two']
+end
+
+def first_element
+  taylor_swift[0]
+end
+
+def third_element
+  taylor_swift[2]
+end
+
+def last_element
+  taylor_swift[-1]
+end
+
+def first_element_with_array_methods
+  south_east_asia.first
+end
+
+def last_element_with_array_methods
+south_east_asia.last
+end
+
+def length_of_array
+  programming_languages.length
+end
